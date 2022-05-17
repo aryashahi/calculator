47716c961d880b7f0e8ab5a6d49cc143354e9145
Arya <aryashahi2010@gmail.com>
2022-05-17 23:00:18 +0530

    new bmi feature added

--- calculator.js
+++ calculator.js
@@ -1,11 +1,37 @@
-const { response } = require( "express" );
+//const { response } = require( "express" );
 const express = require("express");
-const { get } = require( "http" );
+//const { get } = require( "http" );
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+app.get("/bmicalculator", function(req,res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req,res){
+    //res.send("BMI Calculator");
+    var height = parseFloat(req.body.height);
+    var weight = parseFloat(req.body.weight);
+    var bmi = weight/(height*height);
+    res.send("The BMI is " + bmi);
+});
 
 app.get("/",function(req,res){
   res.sendFile(__dirname+"/index.html");
 });
+
+app.post("/",function(req,res){
+
+    var num1 = Number(req.body.number1);
+    var num2 = Number(req.body.number2);
+    var result = num1 + num2; 
+    res.send("The result of the calculation is "+ result);
+});
+
+
 
 app.listen(3000,function(){
     console.log("server running on port 3000");
